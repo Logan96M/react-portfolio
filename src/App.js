d1089ceb4434f04314daaa8ccec547886e5921d7
Logan Morris <loganmorris96@gmail.com>
2020-06-25 22:25:31 -0700

    Added AntD

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Layout, Button } from 'antd';
+import './App.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const { Header } = Layout;
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
+function App() {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-           Logans Portfolio
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+      <>
+    <div classname = 'App'>
+      <Layout>
+          <Header className = 'header'>
+              <h1 className = 'title'> Ant Design Rocks!!! </h1>
+          </Header>
+      </Layout>
+      <Button>Click Me</Button>
     </div>
+    </>
   );
 }
 
-
+export default App
